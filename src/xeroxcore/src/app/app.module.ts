4fdc71d2abe4b1e6af71e3ba17d2fc830c9e658b
Nasar Eddaoui <nasar165@hotmail.com>
2020-02-11 22:24:55 +0700

    completed comming soon

--- src/xeroxcore/src/app/app.module.ts
+++ src/xeroxcore/src/app/app.module.ts
@@ -5,18 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Error404Component } from './error404/error404.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Error404Component,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/xeroxcore/src/app/error404/error404.component.ts
+++ src/xeroxcore/src/app/error404/error404.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-error404',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./error404.component.scss']
 })
 export class Error404Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private titleService: Title) {
+    titleService.setTitle("404 Page can't be found");
   }
 
+  ngOnInit() {}
+
+  public getUrl(): string {
+    return window.location.href;
+  }
+
+  public previusePage(): void {
+    window.history.back();
+  }
 }
