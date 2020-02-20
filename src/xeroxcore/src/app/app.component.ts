bf09bd117b009b38eaa42ceeb251fe65d75ec422
Nasar Eddaoui <nasar165@hotmail.com>
2020-02-20 19:04:19 +0700

    Implment scroll animation.

--- src/xeroxcore/src/app/app.component.ts
+++ src/xeroxcore/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'xeroxcore';
+  ngOnInit() {
+    AOS.init();
+  }
 }
