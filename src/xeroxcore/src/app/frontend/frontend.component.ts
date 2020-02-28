02213b753be026c1a3b68a2a3e182421ec12e9f6
Nasar <Nasar165@hotmail.com>
2020-02-28 14:34:32 +0700

    fixed #19

--- src/xeroxcore/src/app/frontend/frontend.component.ts
+++ src/xeroxcore/src/app/frontend/frontend.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { routerTransition } from './Models/animation';
 
 @Component({
   selector: 'app-frontend',
   templateUrl: './frontend.component.html',
-  styleUrls: ['./frontend.component.scss']
+  styleUrls: ['./frontend.component.scss'],
+  animations: [routerTransition]
 })
 export class FrontendComponent implements OnInit {
   constructor() {
     AOS.init();
+  }
+
+  getState(outlet) {
+    return outlet.activatedRouteData.state;
   }
 
   ngOnInit(): void {}
