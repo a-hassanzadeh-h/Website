34224080d3a20f6c3fe518b7cd5a017756f5936d
nasar eddaoui <nasareddaoui@localhost.localdomain>
2020-07-13 23:00:28 +0700

    fixed #40 Multi Langua support implemented

--- src/xeroxcore/src/app/app.component.ts
+++ src/xeroxcore/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MetaData } from './core/models/metadata';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,10 @@ import { MetaData } from './core/models/metadata';
 export class AppComponent {
   title = 'Xeroxcore';
   date = new Date();
-  constructor(private meta: MetaData) {}
+  constructor(private meta: MetaData, public translate: TranslateService) {
+    translate.addLangs(['en', 'th', 'swe', 'fr']);
+    translate.setDefaultLang('en');
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+  }
 }
--- src/xeroxcore/src/app/app.module.ts
+++ src/xeroxcore/src/app/app.module.ts
@@ -6,10 +6,28 @@ import { AppComponent } from './app.component';
 import { MetaData } from './core/models/metadata';
 import { LocalStorage } from './core/models/localstorage';
 import { DateTimePipe } from './core/pipes/datetimepipe';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [AppComponent, DateTimePipe],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   providers: [MetaData, LocalStorage],
   bootstrap: [AppComponent],
 })
