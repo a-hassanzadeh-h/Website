433299050535a3a3c15dab8ed474ee3c5d58b393
Nasar Eddaoui <nasar165@hotmail.com>
2020-07-28 01:06:46 +0700

    functioning highlights component

--- src/src/app/shared/highlight/highlight.component.ts
+++ src/src/app/shared/highlight/highlight.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Language } from 'src/app/core/models/language.model';
 
 @Component({
   selector: 'app-highlight',
   templateUrl: './highlight.component.html',
-  styleUrls: ['./highlight.component.scss']
+  styleUrls: ['./highlight.component.scss'],
 })
 export class HighlightComponent implements OnInit {
+  constructor(public language: Language) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/src/app/shared/shared.module.ts
+++ src/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CarouselComponent } from './carousel/carousel.component';
 import { CommonModule } from '@angular/common';
 import { HighlightComponent } from './highlight/highlight.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [CarouselComponent, HighlightComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, CoreModule],
   exports: [CarouselComponent, HighlightComponent],
 })
 export class SharedModule {}
